b1be231da45a209c10fc4a3cb4a0977f180a142f
royzding <royzding@gmail.com>
2021-02-21 11:27:04 -0500

    this is first commit.

---
+++ SB-Eureka-Server/src/test/java/com/sb/ek/sv/SbEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.ek.sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
