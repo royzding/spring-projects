d4a2bcd2ca432dbcea9f7b5dffe76704b3934dc8
royzding <royzding@gmail.com>
2021-02-21 21:21:50 -0500

    changes

--- SB-Eureka-Server/src/main/java/com/sb/ek/sv/SbEurekaServerApplication.java
+++ SB-Eureka-Server/src/main/java/com/sb/ek/sv/SbEurekaServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class SbEurekaServerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Test");
 		SpringApplication.run(SbEurekaServerApplication.class, args);
 	}
 
