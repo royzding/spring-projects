58ebb3cf5f9d787afd2ee4faf36edcf2858b886a
royzding <royzding@gmail.com>
2021-03-11 17:36:28 -0500

    changed test pkgs.

--- spring-boot-api-gateway/gateway-service/src/test/java/com/sb/gateway/service/GatewayServiceApplicationTests.java
+++ spring-boot-api-gateway/gateway-service/src/test/java/com/sb/gateway/service/GatewayServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.devglan.gatewayservice;
+package com.sb.gateway.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- spring-boot-api-gateway/spring-boot-course-service/src/test/java/com/sb/course/service/StudentServiceApplicationTests.java
+++ spring-boot-api-gateway/spring-boot-course-service/src/test/java/com/sb/course/service/StudentServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springbootzuulgatwayproxystudentservice;
+package com.sb.course.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
