1f16057296ed0a516b357c69c486d105764a6c1d
royzding <royzding@gmail.com>
2021-03-29 15:52:27 -0400

    added mockito.

--- Spring-RabbitMQ-master/rabbitmq-consumer-yml/src/test/java/com/test/rabbitmq/consumer/RabbitmqConsumerApplicationTests.java
+++ Spring-RabbitMQ-master/rabbitmq-consumer-yml/src/test/java/com/test/rabbitmq/consumer/RabbitmqConsumerApplicationTests.java
@@ -1,9 +1,13 @@
 package com.test.rabbitmq.consumer;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.test.rabbitmq.consumer.model.Payload;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,12 @@ public class RabbitmqConsumerApplicationTests {
 
     @Test
     public void contextLoads() {
+    	
+    	Payload payload = new Payload();
+    	
+    	when(payload.getMessage1()).thenReturn("this is m1");
+    	
+    	
     	
     }
 
