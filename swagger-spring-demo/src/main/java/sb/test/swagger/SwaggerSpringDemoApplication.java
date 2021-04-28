3a21f358efb0e101d345baf6ac60266dc7cb0a86
royzding <royzding@gmail.com>
2021-04-28 15:12:58 -0400

    swagger open api demo

---
+++ swagger-spring-demo/src/main/java/sb/test/swagger/SwaggerSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package sb.test.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSpringDemoApplication.class, args);
+	}
+
+}
