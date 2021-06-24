a368b23a88f482d4d8f921ca00c1437aeda5440e
royzding <royzding@gmail.com>
2021-06-24 16:18:40 -0400

    swagger

--- spring-cloud-config-server/sample-spring-microservices/organization-service/src/main/java/pl/piomin/services/organization/OrganizationApplication.java
+++ spring-cloud-config-server/sample-spring-microservices/organization-service/src/main/java/pl/piomin/services/organization/OrganizationApplication.java
@@ -10,17 +10,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import pl.piomin.services.organization.model.Organization;
 import pl.piomin.services.organization.repository.OrganizationRepository;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableSwagger2
+
 @OpenAPIDefinition(info =
 	@Info(title = "Organization API", version = "1.0", description = "Documentation Organization API v1.0")
 )
@@ -30,16 +24,6 @@ public class OrganizationApplication {
 		SpringApplication.run(OrganizationApplication.class, args);
 	}
 
-	@Bean
-	public Docket swaggerApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-					.apis(RequestHandlerSelectors.basePackage("pl.piomin.services.organization.controller"))
-					.paths(PathSelectors.any())
-				.build()
-				.apiInfo(new ApiInfoBuilder().version("1.0").title("Organization API").description("Documentation Organization API v1.0").build());
-	}
-	
 	@Bean
 	OrganizationRepository repository() {
 		OrganizationRepository repository = new OrganizationRepository();
