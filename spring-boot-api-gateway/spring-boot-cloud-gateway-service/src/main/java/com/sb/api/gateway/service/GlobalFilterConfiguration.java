97f85a9b4394539e08a11560a6a1c46be3d453af
royzding <royzding@gmail.com>
2021-06-24 16:51:47 -0400

    swagger

--- spring-boot-api-gateway/spring-boot-cloud-gateway-service/src/main/java/com/sb/api/gateway/service/GlobalFilterConfiguration.java
+++ spring-boot-api-gateway/spring-boot-cloud-gateway-service/src/main/java/com/sb/api/gateway/service/GlobalFilterConfiguration.java
@@ -1,7 +1,14 @@
 package com.sb.api.gateway.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -13,6 +20,22 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Configuration
 public class GlobalFilterConfiguration {
+	
+	@Autowired
+	RouteDefinitionLocator locator;
+
+	//https://piotrminkowski.com/2020/02/20/microservices-api-documentation-with-springdoc-openapi/
+	@Bean
+	public List<GroupedOpenApi> apis() {
+	   List<GroupedOpenApi> groups = new ArrayList<>();
+	   List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+	   definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+	      String name = routeDefinition.getId().replaceAll("-service", "");
+	      groups.add(GroupedOpenApi.builder().pathsToMatch("/").setGroup(name).build());
+	   });
+	   return groups;
+	}	
+	
 
 	@Bean
 	public GlobalFilter customFilter() {
