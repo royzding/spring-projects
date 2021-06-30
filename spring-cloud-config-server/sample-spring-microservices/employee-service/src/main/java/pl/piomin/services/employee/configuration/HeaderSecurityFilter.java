186eee9e09da78c4946ee936b499eab18575f398
royzding <royzding@gmail.com>
2021-06-30 14:25:37 -0400

    secure micro service employee.

---
+++ spring-cloud-config-server/sample-spring-microservices/employee-service/src/main/java/pl/piomin/services/employee/configuration/HeaderSecurityFilter.java
@@ -0,0 +1,40 @@
+package pl.piomin.services.employee.configuration;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class HeaderSecurityFilter extends OncePerRequestFilter {
+	
+	private String svcKey;
+	private static final String HEADER_SVC_KEY = "X-SVC-KEY";
+	
+	HeaderSecurityFilter(String svcKey) {
+		this.svcKey = svcKey;
+	}
+ 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+                                    throws ServletException, IOException {
+    	
+    	String svcHeader = request.getHeader(HEADER_SVC_KEY);
+    	
+    	System.out.println("=============" + request.getRequestURI());
+    	System.out.println("=============" + svcHeader);
+ 
+        if(svcHeader == null || !svcHeader.equals(this.svcKey)) {
+        	throw new BadCredentialsException("The Service Key was not found!");
+        }
+        
+        filterChain.doFilter(request, response);
+
+    }
+}
--- spring-cloud-config-server/sample-spring-microservices/gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
+++ spring-cloud-config-server/sample-spring-microservices/gateway-service/src/main/java/pl/piomin/services/gateway/GatewayApplication.java
@@ -9,13 +9,22 @@ import org.springdoc.core.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.NegatedServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 
-@SpringBootApplication
+import io.netty.handler.codec.http.HttpMethod;
+
+@SpringBootApplication(exclude= {ReactiveUserDetailsServiceAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableWebFluxSecurity
 public class GatewayApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
@@ -40,4 +49,17 @@ public class GatewayApplication {
 	   return groups;
 	}	
 
+	@Bean
+	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity) {
+		return httpSecurity
+				.csrf()
+				.requireCsrfProtectionMatcher(new NegatedServerWebExchangeMatcher(
+						new PathPatternParserServerWebExchangeMatcher("/v3/**")))
+				.disable()
+				.headers()
+				.contentSecurityPolicy("default-src 'self'; 'strict-dynamic'; frame-ancestors 'none'; script-src 'self' 'unsafe-inline'; ")
+				.and()
+				.and().build();
+		
+	}
 }
