10da5d5765731ee57716ea6350ad4c590d0fe960
royzding <royzding@gmail.com>
2021-07-29 16:19:39 -0400

    swagger config on employee

--- spring-cloud-config-server/sample-spring-microservices/employee-service/src/main/java/pl/piomin/services/employee/controller/EmployeeController.java
+++ spring-cloud-config-server/sample-spring-microservices/employee-service/src/main/java/pl/piomin/services/employee/controller/EmployeeController.java
@@ -11,6 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import pl.piomin.services.employee.model.Employee;
 import pl.piomin.services.employee.repository.EmployeeRepository;
 
@@ -27,6 +33,16 @@ public class EmployeeController {
 		LOGGER.info("Employee find");
 		return "Hi Employee";
 	}
+	
+	@Operation(summary="Create an employee")
+	@ApiResponses(value= {
+			@ApiResponse(responseCode="201",description="Employee created",
+					content= {@Content(mediaType="application/json", schema=@Schema(implementation=Employee.class)) }),
+			@ApiResponse(responseCode="400",description="Bad request"), 
+			@ApiResponse(responseCode="409",description="Employee with the same name already exists"), 
+			@ApiResponse(responseCode="500",description="Internal Server Error. The server could not process the request",
+			content= @Content) 
+	})
 
 	@PostMapping("/")
 	public Employee add(@RequestBody Employee employee) {
@@ -46,6 +62,12 @@ public class EmployeeController {
 		return repository.findAll();
 	}
 	
+	@Operation(summary="get a list of employee of the department")
+	@ApiResponses(value= {
+		@ApiResponse(responseCode="200",description="Success. An empty list is returned when no records are found",
+					content= {@Content(mediaType="application/json", array=@ArraySchema(schema=@Schema(implementation=Employee.class))) }),
+		@ApiResponse(responseCode="500",description="Internal Server Error. The server could not process the request",content= @Content) 
+	})
 	@GetMapping("/department/{departmentId}")
 	public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId) {
 		LOGGER.info("Employee find: departmentId={}", departmentId);
