cabf4aafae6df4553cfde881759d353ea60ebf5b
royzding <roy@DESKTOP-NAS07GD>
2021-09-06 14:45:33 -0400

    common ware

---
+++ spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/java/com/sample/microservices/common/ware/CommonWareApplication.java
@@ -0,0 +1,13 @@
+package com.sample.microservices.common.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonWareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonWareApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/java/com/sample/microservices/common/ware/model/UserInfo.java
@@ -0,0 +1,11 @@
+package com.sample.microservices.common.ware.model;
+
+import lombok.Data;
+
+@Data
+public class UserInfo {
+
+	private String firstName;
+	private String lastName;
+	private int age;
+}
