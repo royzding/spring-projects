151e537fe4d9a4b6bbc6ec71708f11bb205172de
royzding <roy@DESKTOP-NAS07GD>
2021-09-06 14:55:55 -0400

    test personal access tokens

--- spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/java/com/sample/microservices/common/ware/CommonWareApplication.java
+++ spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/java/com/sample/microservices/common/ware/CommonWareApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommonWareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommonWareApplication.class, args);
+		SpringApplication.run(CommonWareApplication.class, args); 
 	}
 
 }
