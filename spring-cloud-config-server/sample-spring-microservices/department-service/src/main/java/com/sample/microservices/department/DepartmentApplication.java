6898897582a90c734f6176e63f5f942ce0a9a2d8
royzding <roy@DESKTOP-NAS07GD>
2021-09-16 17:34:33 -0400

    added scanBasePackages.

--- spring-cloud-config-server/sample-spring-microservices/department-service/src/main/java/com/sample/microservices/department/DepartmentApplication.java
+++ spring-cloud-config-server/sample-spring-microservices/department-service/src/main/java/com/sample/microservices/department/DepartmentApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages= {"com.sample.microservices.department","com.sample.microservices.common"})
+@SpringBootApplication(scanBasePackages= {"com.sample.microservices"})
 @EnableDiscoveryClient
 @EnableFeignClients
 public class DepartmentApplication {
