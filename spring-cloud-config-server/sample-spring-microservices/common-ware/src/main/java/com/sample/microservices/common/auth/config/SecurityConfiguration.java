93846392f2a3b393ee5032073a0ff98619f463a5
royzding <roy@DESKTOP-NAS07GD>
2021-09-21 16:03:21 -0400

    api-gateway apiKey.

--- spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/java/com/sample/microservices/common/auth/config/SecurityConfiguration.java
+++ spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/java/com/sample/microservices/common/auth/config/SecurityConfiguration.java
@@ -21,7 +21,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		
-    	System.out.println("================== get into" + SecurityConfiguration.class);
+    	System.out.println("$$$$$$$$$$$$$$$$$$$$$ get into svcKey==" + svcKey);
+    	System.out.println("$$$$$$$$$$$$$$$$$$$$$ get into" + SecurityConfiguration.class);
 
 		http
 		.addFilterBefore(new HeaderSecurityFilter(svcKey), BasicAuthenticationFilter.class)
--- spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/java/com/sample/microservices/common/auth/filter/HeaderSecurityFilter.java
+++ spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/java/com/sample/microservices/common/auth/filter/HeaderSecurityFilter.java
@@ -7,13 +7,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class HeaderSecurityFilter extends OncePerRequestFilter {
 	
-	private String svcKey;
 	private static final String HEADER_SVC_KEY = "X-SVC-KEY";
+
+	private String svcKey;
 	
 	public HeaderSecurityFilter(String svcKey) {
 		this.svcKey = svcKey;
@@ -27,13 +29,14 @@ public class HeaderSecurityFilter extends OncePerRequestFilter {
     	
     	String svcHeader = request.getHeader(HEADER_SVC_KEY);
     	
+    	System.out.println("====svcKey=========" + svcKey);
     	System.out.println("=============" + request.getRequestURI());
-    	System.out.println("=============" + svcHeader);
-/*    	 
+    	System.out.println("=====svcHeader========" + svcHeader);
+    	 
         if(svcHeader == null || !svcHeader.equals(this.svcKey)) {
         	throw new BadCredentialsException("The Service Key was not found!");
         }
-*/
+
         filterChain.doFilter(request, response);
 
     }
