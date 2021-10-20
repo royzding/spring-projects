b0853a11db65b61aaf916979ba1d620bcc6e6395
royzding <roy@DESKTOP-NAS07GD>
2021-10-20 17:47:43 -0400

    sql scripts

---
+++ spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/resources/sql-scripts/Employee_Manager.sql
@@ -0,0 +1,156 @@
+
+DROP TABLE person;
+DROP TABLE employee;
+DROP TABLE manager;
+DROP TABLE department;
+DROP TABLE user_table;
+
+CREATE TABLE user_table (
+  user_id 			INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_name 		varchar(255) NULL,
+  created_by 		VARCHAR (255) NOT NULL,
+  created_date 		DATE NOT NULL  
+);
+
+CREATE TABLE person (
+     id 			INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+     name  			VARCHAR2(50) NOT NULL,
+     country    	VARCHAR2(50)
+);
+
+CREATE TABLE manager (
+     id 			INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+     name  			VARCHAR2(50) NOT NULL,
+     salary 		NUMBER(10,2)
+);
+
+CREATE TABLE department (
+     id 			INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+     name  			VARCHAR2(50) NOT NULL
+);
+
+CREATE TABLE employee (
+     id 					INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+     firstname  			VARCHAR2(50) NOT NULL,
+	 lastname				VARCHAR2(50) NOT NULL,
+     dep_id 				NUMBER,
+     manager_id 			NUMBER,
+     salary 				NUMBER(10,2),
+     designation    		VARCHAR2(50),
+	 CONSTRAINT fk_dep_id FOREIGN KEY (dep_id) REFERENCES department(id),
+	 CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES manager(id)
+);
+
+
+////create department:
+[
+  {
+    "name": "dep-1"
+  },
+  {
+    "name": "dep-2"
+  },
+  {
+    "name": "dep-3"
+  }
+]
+
+////create manager:
+[
+  {
+    "name": "manager-1",
+	"salary": 10
+  },
+  {
+    "name": "manager-2",
+	"salary": 20
+  },
+  {
+    "name": "manager-3",
+	"salary": 30
+  }
+]
+
+///create employee:
+[
+	{
+	  "id": 0,
+	  "firstName": "fn1",
+	  "lastName": "ln1",
+	  "salary": 10,
+	  "depId": 1,
+	  "managerId": 4
+	},
+	{
+	  "id": 0,
+	  "firstName": "fn2",
+	  "lastName": "ln2",
+	  "salary": 20,
+	  "depId": 2,
+	  "managerId": 4
+	},
+	{
+	  "id": 0,
+	  "firstName": "fn3",
+	  "lastName": "ln3",
+	  "salary": 30,
+	  "depId": 2,
+	  "managerId": 5
+	},
+	{
+	  "id": 0,
+	  "firstName": "fn4",
+	  "lastName": "ln4",
+	  "salary": 20,
+	  "depId": 3,
+	  "managerId": 6
+	},
+	{
+	  "id": 0,
+	  "firstName": "fn5",
+	  "lastName": "ln5",
+	  "salary": 20,
+	  "depId": 3,
+	  "managerId": 6
+	},
+	{
+	  "id": 0,
+	  "firstName": "fn6",
+	  "lastName": "ln6",
+	  "salary": 20,
+	  "depId": 3,
+	  "managerId": 6
+	}
+]
+
+
+insert into person (id,name,country) values (1, 'John', 	'USA');
+insert into person (id,name,country) values (2, 'Jane', 	'CHINA');
+insert into person (id,name,country) values (3, 'Steve', 	'UK');
+insert into person (id,name,country) values (4, 'Bruce', 	'GERMAN');
+insert into person (id,name,country) values (5, 'Lina', 	'JAPAN');
+insert into person (id,name,country) values (6, 'Wang', 	'CHINA');
+
+
+insert into employee (id,firstname,lastname,dep_id,salary) values (1, 'John', 'l1', 2, 1200);
+insert into employee (id,firstname,lastname,dep_id,salary) values (2, 'Jane', 'l2', 2, 2200);
+insert into employee (id,firstname,lastname,dep_id,salary) values (3, 'Steve', 'l3', 2, 1800);
+insert into employee (id,firstname,lastname,dep_id,salary) values (4, 'Bruce', 'l4', 2, 3200);
+insert into employee (id,firstname,lastname,dep_id,salary) values (5, 'Lina', 'l5', 2, 5200);
+insert into employee (id,firstname,lastname,dep_id,salary) values (6, 'Wang', 'l6',	3, 6900);
+insert into employee (id,firstname,lastname,dep_id,salary) values (7, 'Zhan', 'l7', 2, 1500);
+insert into employee (id,firstname,lastname,dep_id,salary) values (8, 'Liu',   'l8',	5, 6900);
+insert into employee (id,firstname,lastname,dep_id,salary) values (9, 'Chen', 'l9', 2, 3400);
+insert into employee (id,firstname,lastname,dep_id,salary) values (10, 'Ding', 'l10', 2, 4200);
+
+insert into manager (id,name,salary) values (1, 'Marc', 1200);
+insert into manager (id,name,salary) values (2, 'Luke', 2200);
+insert into manager (id,name,salary) values (3, 'Emily', 1800);
+
+insert into department (id,name) values (2, 'Computer Science');
+insert into department (id,name) values (3, 'Chemistry');
+insert into department (id,name) values (5, 'Physics');
+
+commit;
+
+
