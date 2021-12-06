83dd8f1cfcb7c433e68f751444b453419d366018
royzding <roy@DESKTOP-NAS07GD>
2021-12-06 15:54:25 -0500

    review questions.

---
+++ spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/java/com/sample/microservices/common/Test.java
@@ -0,0 +1,29 @@
+package com.sample.microservices.common;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		var $_= 6_000;
+		var $2= 6_000f;
+		
+		var var= 3_0_00.0;
+		
+		System.out.println("test " + $_ + " "+ $2 + " " + var);
+		
+		long reptile = 192301398193810323l;
+		
+		int x = 1;
+		
+		System.out.println(x=5);
+		
+		boolean a = true;
+		
+		boolean bx = (boolean)a;
+		
+		
+
+
+	}
+
+}
