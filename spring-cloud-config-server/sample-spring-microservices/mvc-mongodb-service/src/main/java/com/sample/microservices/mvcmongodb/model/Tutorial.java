ba34e8e1a9386881318dbb89036c70d3646fec34
royzding <roy@DESKTOP-NAS07GD>
2022-02-10 12:06:22 -0500

    @JsonProperty annotation.

--- spring-cloud-config-server/sample-spring-microservices/mvc-mongodb-service/src/main/java/com/sample/microservices/mvcmongodb/model/Tutorial.java
+++ spring-cloud-config-server/sample-spring-microservices/mvc-mongodb-service/src/main/java/com/sample/microservices/mvcmongodb/model/Tutorial.java
@@ -1,7 +1,11 @@
 package com.sample.microservices.mvcmongodb.model;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
@@ -11,6 +15,7 @@ public class Tutorial {
   @Id
   private String id;
 
+  @JsonProperty("TUTORIAL_TITLE")
   private String title;
   private String description;
   private boolean published;
@@ -23,6 +28,11 @@ public class Tutorial {
     this.title = title;
     this.description = description;
     this.published = published;
+  }
+  
+  @JsonProperty("CURRENT_TIME")
+  public String getCurrentTime() {
+	  return LocalDateTime.now().toString();
   }
 
 }
