8bad28786845682c93c314473530b0282d081690
royzding <roy@DESKTOP-NAS07GD>
2022-03-18 19:55:51 -0400

    api-gateway swagger for springdoc-openapi-webflux-ui version 1.6.4

--- spring-cloud-config-server/sample-spring-microservices/gateway-service/src/main/java/com/sample/microservices/gateway/GatewayApplication.java
+++ spring-cloud-config-server/sample-spring-microservices/gateway-service/src/main/java/com/sample/microservices/gateway/GatewayApplication.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
@@ -14,13 +14,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.util.matcher.NegatedServerWebExchangeMatcher;
 import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
-
-import io.netty.handler.codec.http.HttpMethod;
 
 @SpringBootApplication(exclude= {ReactiveUserDetailsServiceAutoConfiguration.class},
 	scanBasePackages= {"com.sample.microservices.gateway","com.sample.microservices.common"})
@@ -34,22 +33,46 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+
+/*	
+	//http://localhost:8889/swagger-ui.html
+	//https://piotrminkowski.com/2020/02/20/microservices-api-documentation-with-springdoc-openapi/
+	//old springdoc-openapi-webflux-ui version 1.2.33
+	 
 	@Autowired
 	RouteDefinitionLocator locator;
 
-	//http://localhost:8889/swagger-ui.html
-	//https://piotrminkowski.com/2020/02/20/microservices-api-documentation-with-springdoc-openapi/
 	@Bean
 	public List<GroupedOpenApi> apis() {
 	   List<GroupedOpenApi> groups = new ArrayList<>();
 	   List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
 	   definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
 	      String name = routeDefinition.getId().replaceAll("-service", "");
-	      groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").setGroup(name).build());
+	      groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
 	   });
 	   return groups;
 	}	
 
+*/
+	
+	//new springdoc-openapi-webflux-ui version 1.6.4
+	
+	@Bean
+	@Lazy(false)
+	public List<GroupedOpenApi> apis(SwaggerUiConfigParameters swaggerUiConfigParameters, RouteDefinitionLocator locator) {
+		List<GroupedOpenApi> groups = new ArrayList<>();
+		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+		for (RouteDefinition definition : definitions) {
+			System.out.println("id: " + definition.getId()+ "  "+definition.getUri().toString());
+		}
+		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+			String name = routeDefinition.getId().replaceAll("-service", "");
+			swaggerUiConfigParameters.addGroup(name);
+			GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+		});
+		return groups;
+	}
+	
 	@Bean
 	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity) {
 		return httpSecurity
