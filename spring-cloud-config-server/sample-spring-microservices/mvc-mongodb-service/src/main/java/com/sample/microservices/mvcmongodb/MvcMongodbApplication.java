02f3f6a2c4a90a7fc76b3c4539237f2dc030ba17
royzding <roy@DESKTOP-NAS07GD>
2022-03-22 11:32:31 -0400

    mvc mongodb dev updates.

--- spring-cloud-config-server/sample-spring-microservices/mvc-mongodb-service/src/main/java/com/sample/microservices/mvcmongodb/MvcMongodbApplication.java
+++ spring-cloud-config-server/sample-spring-microservices/mvc-mongodb-service/src/main/java/com/sample/microservices/mvcmongodb/MvcMongodbApplication.java
@@ -4,8 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication(scanBasePackages= {"com.sample.microservices.mvcmongodb","com.sample.microservices.common"})
 @EnableDiscoveryClient
+@OpenAPIDefinition(
+		servers = { 
+			@Server(url = "http://localhost:8082/mvc-mongodb"),
+			@Server(url = "http://localhost:8085/mvc-mongodb") 
+		}, 
+		info = @Info(title = "mvc-mongodb-service", 
+		version = "${springdoc.open-api.version}", 
+		description = "mvc-mongodb-Service APIs", 
+		license = @License(name = "${springdoc.open-api.license.name}", url = "http://foo.bar"), 
+		contact = @Contact(url = "http://mvc-mongodb-server.com", 
+		name = "${springdoc.open-api.name}", 
+		email = "${springdoc.open-api.email}"))
+)
 public class MvcMongodbApplication {
 
 	public static void main(String[] args) {
