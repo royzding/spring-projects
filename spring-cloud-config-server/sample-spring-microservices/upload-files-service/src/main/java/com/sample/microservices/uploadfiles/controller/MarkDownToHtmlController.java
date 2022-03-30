e57a721d1494faa3dad4343dff04ebc816cac810
royzding <roy@DESKTOP-NAS07GD>
2022-03-30 10:09:25 -0400

    markdown to html in spring boot.

---
+++ spring-cloud-config-server/sample-spring-microservices/upload-files-service/src/main/java/com/sample/microservices/uploadfiles/controller/MarkDownToHtmlController.java
@@ -0,0 +1,32 @@
+package com.sample.microservices.uploadfiles.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.microservices.uploadfiles.service.MarkDownToHtmlService;
+
+@RestController
+@RequestMapping("/markdown-to-html")
+public class MarkDownToHtmlController {
+
+  @Autowired
+  MarkDownToHtmlService  markDownToHtmlService;
+
+  @GetMapping(value="/{filename}")
+  public String deleteDirs(@PathVariable("filename") String filename) throws IOException {
+        
+	  File file = ResourceUtils.getFile("classpath:" + filename);
+        
+	  return markDownToHtmlService.markdownToHtml(new String(Files.readAllBytes(file.toPath())));
+  }
+  
+
+}
