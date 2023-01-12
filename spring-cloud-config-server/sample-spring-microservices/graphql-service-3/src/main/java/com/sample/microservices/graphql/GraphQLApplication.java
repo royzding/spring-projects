450df4f08a57729dcdcb4ce39454cfd00f817701
royzding <roy@DESKTOP-NAS07GD>
2023-01-12 16:27:43 -0500

    mutiple queries

---
+++ spring-cloud-config-server/sample-spring-microservices/graphql-service-3/src/main/java/com/sample/microservices/graphql/GraphQLApplication.java
@@ -0,0 +1,13 @@
+package com.sample.microservices.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class GraphQLApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQLApplication.class, args);
+    }
+}
