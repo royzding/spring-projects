7bf68fb05759aa57087f71377dcdb5b60ad7fa2f
royzding <roy@DESKTOP-NAS07GD>
2023-02-06 10:11:15 -0500

    added switch to pl/sql

--- spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/resources/files/schema/daylight_trigger.sql
+++ spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/resources/files/schema/daylight_trigger.sql
@@ -20,7 +20,7 @@ CREATE TABLE application_properties
 
 );
 
-INSERT INTO application_properties (ID, KEY, VALUE) VALUES (100, 'DATLIGHT_SAVING_TIME', 'WINTER');
+INSERT INTO application_properties (ID, KEY, A_TIME) VALUES (100, 'DATLIGHT_SAVING_TIME', 'WINTER');
 
 DROP TABLE daylight_saving_time_update;
 
@@ -70,61 +70,20 @@ INSERT INTO daylight_saving_time_update_2 (ID, NAME, A_TIME_2) VALUES (109, 'tim
 
 COMMIT;
 
-CREATE OR REPLACE PROCEDURE p_insert_two_param_plus_one
+CREATE OR REPLACE PROCEDURE p_insert_two_param_plus_one(description IN VARCHAR2)
 IS
 BEGIN
 
-	p_insert_two_param(1, 'p_insert_two_param_plus_one');
+	p_insert_two_param(1, description);
 
 END;
 /
 
-CREATE OR REPLACE PROCEDURE p_insert_two_param_minus_one
+CREATE OR REPLACE PROCEDURE p_insert_two_param_minus_one(description IN VARCHAR2)
 IS
 BEGIN
 
-	p_insert_two_param(-1, 'p_insert_two_param_minus_one');
-
-END;
-/
-
-
-
-CREATE OR REPLACE PROCEDURE p_insert_two_param(delta IN NUMBER, description IN VARCHAR2 )
-
-IS
-	CURSOR c_dstu is SELECT ID, A_TIME FROM daylight_saving_time_update;
-
-	CURSOR c_dstu_2 is SELECT ID, A_TIME_2 FROM daylight_saving_time_update_2;
-
-BEGIN
-
-   insert into daylight_saving_update (delta, description, modified_date) values (delta, description, CURRENT_TIMESTAMP);
-
-   FOR dls_time in c_dstu
-   LOOP
-
-		UPDATE daylight_saving_time_update 
-	      	SET A_TIME = f_convert_daylight_time(delta, dls_time.A_TIME),
-	      	MODIFIED_DATE =  CURRENT_TIMESTAMP
-	    WHERE ID = dls_time.ID;  
-
-   END LOOP;
-
-   FOR dls_time_2 in c_dstu_2
-   LOOP
-
-		UPDATE daylight_saving_time_update_2 
-	      	SET A_TIME_2 = f_convert_daylight_time(delta, dls_time_2.A_TIME_2),
-	      	MODIFIED_DATE =  CURRENT_TIMESTAMP
-	    WHERE ID = dls_time_2.ID;  
-
-   END LOOP;
-
-EXCEPTION
-
-WHEN OTHERS THEN
-   raise_application_error(-20001,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+	p_insert_two_param(-1, description);
 
 END;
 /
@@ -134,46 +93,62 @@ END;
 CREATE OR REPLACE PROCEDURE p_insert_two_param(delta IN NUMBER, description IN VARCHAR2 )
 
 IS
+    SWITCH_STR		VARCHAR2(30);
+    
 	CURSOR c_dstu is SELECT ID, A_TIME FROM daylight_saving_time_update;
-
 	CURSOR c_dstu_2 is SELECT ID, A_TIME_2 FROM daylight_saving_time_update_2;
 
 BEGIN
 
-   insert into daylight_saving_update (delta, description, modified_date) values (delta, description, CURRENT_TIMESTAMP);
+   SAVEPOINT TR_BEGIN;
+   
+   SWITCH_STR := UPPER(TRIM(description));
+   
+   IF SWITCH_STR='ALL' OR SWITCH_STR='DSTU1' THEN
 
-   FOR dls_time in c_dstu
-   LOOP
-
-		UPDATE daylight_saving_time_update 
-	      	SET A_TIME = f_convert_daylight_time(delta, dls_time.A_TIME),
-	      	MODIFIED_DATE =  CURRENT_TIMESTAMP
-	    WHERE ID = dls_time.ID;  
-
-   END LOOP;
-
-   FOR dls_time_2 in c_dstu_2
-   LOOP
-
-		UPDATE daylight_saving_time_update_2 
-	      	SET A_TIME_2 = f_convert_daylight_time(delta, dls_time_2.A_TIME_2),
-	      	MODIFIED_DATE =  CURRENT_TIMESTAMP
-	    WHERE ID = dls_time_2.ID;  
-
-   END LOOP;
+	   FOR dls_time in c_dstu
+	   LOOP
+	
+			UPDATE daylight_saving_time_update 
+		      	SET A_TIME = f_convert_daylight_time(delta, dls_time.A_TIME),
+		      	MODIFIED_DATE =  CURRENT_TIMESTAMP
+		    WHERE ID = dls_time.ID;  
+	
+	   END LOOP;
+	
+   END IF;
+	
+   IF SWITCH_STR='ALL' OR SWITCH_STR='DSTU2' THEN
+	
+	   FOR dls_time_2 in c_dstu_2
+	   LOOP
+	
+			UPDATE daylight_saving_time_update_2 
+		      	SET A_TIME_2 = f_convert_daylight_time(delta, dls_time_2.A_TIME_2),
+		      	MODIFIED_DATE =  CURRENT_TIMESTAMP
+		    WHERE ID = dls_time_2.ID;  
+	
+	   END LOOP;
+	   
+   END IF;
+   
+   COMMIT;
 
 EXCEPTION
 
 WHEN OTHERS THEN
+
+   ROLLBACK TO TR_BEGIN;
+   
    raise_application_error(-20001,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
 
 END;
 /
-
 
 CREATE OR REPLACE Function f_convert_daylight_time  ( delta IN NUMBER, a_time IN varchar2 ) RETURN VARCHAR2
 
 IS
+	T_TIME_STR		VARCHAR2(100);
 	A_TIME_HOUR_STR	VARCHAR2(50);
 	A_TIME_MIN_STR	VARCHAR2(50);
 	A_TIME_HOUR  	INTEGER;
@@ -192,8 +167,10 @@ BEGIN
 	      CONNECT BY REGEXP_SUBSTR (a_time, '[^;]+', 1, LEVEL) IS NOT NULL)
 	   LOOP
 	   
-	      A_TIME_HOUR_STR := SUBSTR( TRIM(S_TIME.TIME_STR), 1, 2);
-	      A_TIME_MIN_STR  := SUBSTR( TRIM(S_TIME.TIME_STR), 4, 2);
+	   	  T_TIME_STR := TRIM(S_TIME.TIME_STR);
+	   
+	      A_TIME_HOUR_STR := SUBSTR( T_TIME_STR, 1, INSTR(T_TIME_STR, ':', 1, 1)-1 );
+	      A_TIME_MIN_STR  := SUBSTR( T_TIME_STR, INSTR(T_TIME_STR, ':', 1, 1) );
 	
 	      A_TIME_HOUR := TO_NUMBER( A_TIME_HOUR_STR, '99') + delta;
 	      
@@ -207,7 +184,7 @@ BEGIN
 	      	
 	      END IF;
 	
-		  C_TIME := C_TIME || ';' || TO_CHAR(A_TIME_HOUR, 'fm00') || ':' || A_TIME_MIN_STR;
+		  C_TIME := C_TIME || ';' || TO_CHAR(A_TIME_HOUR, 'fm00') || A_TIME_MIN_STR;
 	   
 	   END LOOP;
 	   
@@ -230,84 +207,6 @@ WHEN OTHERS THEN
 END;
 /
 
-
-
----- call P_INSERT_TWO_PARAM (1, 'test');
-call p_insert_two_param_plus_one();
-
-call p_insert_two_param_minus_one();
-
-
-INSERT INTO daylight_saving_update VALUES (23,SYSDATE,2);
-
-CREATE OR REPLACE PROCEDURE P_DAYLIGHT_SAVING_TRIG_JOB
-IS
-	ld_date date;
-BEGIN
-	SELECT B INTO ld_date FROM daylight_saving_update;
-	
-	IF to_char(ld_date,'DD-MM-YYYY') = to_char(sysdate,'DD-MM-YYYY') THEN
-	
-	UPDATE daylight_saving_update SET C = A + 1;
-	INSERT INTO daylight_saving_update VALUES (23,SYSDATE,2);
-	
-	END IF;
-END;
-
-
-DECLARE
-	ln_job_no number;
-BEGIN
-
-	dbms_job.submit(ln_job_no,'P_DAYLIGHT_SAVING_TRIG_JOB;',SYSDATE,'SYSDATE+1',NULL);
-	
-	dbms_job.run(ln_job_no);
-	
-END;
-
-
-
--- job package using
-
--- create a table
-CREATE TABLE TEMP(ID NUMBER, NAME VARCHAR2(25));
-
--- write a pl/sql procedure to insert a new row in the temp tble each time when the job is called.
-
-CREATE OR REPLACE PROCEDURE TEMPPROCEDURE AS
-BEGIN
-	insert into temp values(1,'khaleel');
-COMMIT;
-END;
-/
--- prepare and start the job.
-
-CREATE OR REPLACE PROCEDURE jobpro1
-AS
-	v_jobno NUMBER ;
-	v_what VARCHAR2(2000) := 'TEMPPROCEDURE;';
-	v_interval VARCHAR2(200) := 'SYSDATE+1/(24*60)'; -- to execute after every one munite once
-BEGIN
-
-	DBMS_JOB.SUBMIT(job => v_jobno,
-					what => v_what,
-					next_date => SYSDATE,
-					interval =>v_interval);
-	commit;
-	
-	dbms_output.put_line('job Number : '||v_jobno);
-
-END;
-
-
-
--- droping a job is
-exec dbms_job.remove(42); -- 42 is job number
--- stopping a job
-exec dbms_job.broken(42,true) -- to stop the running
--- to restart the job
-exec dbms_job.broken(42,false) --
--- to run the job without considering its next trip
-exec dbms_job.run(42)
-
--- end of the job submitting procedure
+---- ALL, DSTU1, or DSTU1
+---- call p_insert_two_param_plus_one('ALL');
+---- call p_insert_two_param_minus_one('DSTU1');
