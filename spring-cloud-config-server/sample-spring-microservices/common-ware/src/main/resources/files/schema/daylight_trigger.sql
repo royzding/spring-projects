e2d700faa0f2224c0758309f4da4f2fc4a20b50e
royzding <roy@DESKTOP-NAS07GD>
2023-02-10 09:42:15 -0500

    batch service upgrade.

--- spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/resources/files/schema/daylight_trigger.sql
+++ spring-cloud-config-server/sample-spring-microservices/common-ware/src/main/resources/files/schema/daylight_trigger.sql
@@ -70,81 +70,6 @@ INSERT INTO daylight_saving_time_update_2 (ID, NAME, A_TIME_2) VALUES (109, 'tim
 
 COMMIT;
 
-CREATE OR REPLACE PROCEDURE p_insert_two_param_plus_one(description IN VARCHAR2)
-IS
-BEGIN
-
-	p_insert_two_param(1, description);
-
-END;
-/
-
-CREATE OR REPLACE PROCEDURE p_insert_two_param_minus_one(description IN VARCHAR2)
-IS
-BEGIN
-
-	p_insert_two_param(-1, description);
-
-END;
-/
-
-
-
-CREATE OR REPLACE PROCEDURE p_insert_two_param(delta IN NUMBER, description IN VARCHAR2 )
-
-IS
-    SWITCH_STR		VARCHAR2(30);
-    
-	CURSOR c_dstu is SELECT ID, A_TIME FROM daylight_saving_time_update;
-	CURSOR c_dstu_2 is SELECT ID, A_TIME_2 FROM daylight_saving_time_update_2;
-
-BEGIN
-
-   SAVEPOINT TR_BEGIN;
-   
-   SWITCH_STR := UPPER(TRIM(description));
-   
-   IF SWITCH_STR='ALL' OR SWITCH_STR='DSTU1' THEN
-
-	   FOR dls_time in c_dstu
-	   LOOP
-	
-			UPDATE daylight_saving_time_update 
-		      	SET A_TIME = f_convert_daylight_time(delta, dls_time.A_TIME),
-		      	MODIFIED_DATE =  CURRENT_TIMESTAMP
-		    WHERE ID = dls_time.ID;  
-	
-	   END LOOP;
-	
-   END IF;
-	
-   IF SWITCH_STR='ALL' OR SWITCH_STR='DSTU2' THEN
-	
-	   FOR dls_time_2 in c_dstu_2
-	   LOOP
-	
-			UPDATE daylight_saving_time_update_2 
-		      	SET A_TIME_2 = f_convert_daylight_time(delta, dls_time_2.A_TIME_2),
-		      	MODIFIED_DATE =  CURRENT_TIMESTAMP
-		    WHERE ID = dls_time_2.ID;  
-	
-	   END LOOP;
-	   
-   END IF;
-   
-   COMMIT;
-
-EXCEPTION
-
-WHEN OTHERS THEN
-
-   ROLLBACK TO TR_BEGIN;
-   
-   raise_application_error(-20001,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
-
-END;
-/
-
 CREATE OR REPLACE Function f_convert_daylight_time  ( delta IN NUMBER, a_time IN varchar2 ) RETURN VARCHAR2
 
 IS
@@ -207,6 +132,166 @@ WHEN OTHERS THEN
 END;
 /
 
+CREATE OR REPLACE PROCEDURE p_insert_two_param(delta IN NUMBER, description IN VARCHAR2 )
+
+IS
+    SWITCH_STR		VARCHAR2(30);
+    
+	CURSOR c_dstu is SELECT ID, A_TIME FROM daylight_saving_time_update;
+	CURSOR c_dstu_2 is SELECT ID, A_TIME_2 FROM daylight_saving_time_update_2;
+
+BEGIN
+
+   SAVEPOINT TR_BEGIN;
+   
+   SWITCH_STR := UPPER(TRIM(description));
+   
+   IF SWITCH_STR='ALL' OR SWITCH_STR='DSTU1' THEN
+
+	   FOR dls_time in c_dstu
+	   LOOP
+	
+			UPDATE daylight_saving_time_update 
+		      	SET A_TIME = f_convert_daylight_time(delta, dls_time.A_TIME),
+		      	MODIFIED_DATE =  CURRENT_TIMESTAMP
+		    WHERE ID = dls_time.ID;  
+	
+	   END LOOP;
+	
+   END IF;
+	
+   IF SWITCH_STR='ALL' OR SWITCH_STR='DSTU2' THEN
+	
+	   FOR dls_time_2 in c_dstu_2
+	   LOOP
+	
+			UPDATE daylight_saving_time_update_2 
+		      	SET A_TIME_2 = f_convert_daylight_time(delta, dls_time_2.A_TIME_2),
+		      	MODIFIED_DATE =  CURRENT_TIMESTAMP
+		    WHERE ID = dls_time_2.ID;  
+	
+	   END LOOP;
+	   
+   END IF;
+   
+   COMMIT;
+
+EXCEPTION
+
+WHEN OTHERS THEN
+
+   ROLLBACK TO TR_BEGIN;
+   
+   raise_application_error(-20001,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+
+END;
+/
+
+
+CREATE OR REPLACE PROCEDURE p_insert_two_param_plus_one(description IN VARCHAR2)
+IS
+BEGIN
+
+	p_insert_two_param(1, description);
+
+END;
+/
+
+CREATE OR REPLACE PROCEDURE p_insert_two_param_minus_one(description IN VARCHAR2)
+IS
+BEGIN
+
+	p_insert_two_param(-1, description);
+
+END;
+/
+
+
+
+
 ---- ALL, DSTU1, or DSTU1
 ---- call p_insert_two_param_plus_one('ALL');
 ---- call p_insert_two_param_minus_one('DSTU1');
+
+
+
+---- anonymous PL/SQL block
+
+DECLARE
+  l_message VARCHAR2( 255 ) := 'Hello World!';
+BEGIN
+  DBMS_OUTPUT.PUT_LINE( l_message );
+END;
+/
+
+--------------------------------
+
+DECLARE
+      v_result NUMBER;
+BEGIN
+   v_result := 1 / 0;
+   EXCEPTION
+      WHEN ZERO_DIVIDE THEN
+         DBMS_OUTPUT.PUT_LINE( SQLERRM );
+END;
+/
+
+---------------------------------------------
+
+DECLARE
+
+	description 	VARCHAR2(30) := 'ALL';
+	delta			NUMBER		 := 1;
+    SWITCH_STR		VARCHAR2(30);
+    
+	CURSOR c_dstu is SELECT ID, A_TIME FROM daylight_saving_time_update;
+	CURSOR c_dstu_2 is SELECT ID, A_TIME_2 FROM daylight_saving_time_update_2;
+
+BEGIN
+
+   SAVEPOINT TR_BEGIN;
+   
+   SWITCH_STR := UPPER(TRIM(description));
+   
+   IF SWITCH_STR='ALL' OR SWITCH_STR='DSTU1' THEN
+
+	   FOR dls_time in c_dstu
+	   LOOP
+	
+			UPDATE daylight_saving_time_update 
+		      	SET A_TIME = f_convert_daylight_time(delta, dls_time.A_TIME),
+		      	MODIFIED_DATE =  CURRENT_TIMESTAMP
+		    WHERE ID = dls_time.ID;  
+	
+	   END LOOP;
+	
+   END IF;
+	
+   IF SWITCH_STR='ALL' OR SWITCH_STR='DSTU2' THEN
+	
+	   FOR dls_time_2 in c_dstu_2
+	   LOOP
+	
+			UPDATE daylight_saving_time_update_2 
+		      	SET A_TIME_2 = f_convert_daylight_time(delta, dls_time_2.A_TIME_2),
+		      	MODIFIED_DATE =  CURRENT_TIMESTAMP
+		    WHERE ID = dls_time_2.ID;  
+	
+	   END LOOP;
+	   
+   END IF;
+   
+   COMMIT;
+
+EXCEPTION
+
+WHEN OTHERS THEN
+
+   ROLLBACK TO TR_BEGIN;
+   
+   raise_application_error(-20001,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+
+END;
+/
+
+
